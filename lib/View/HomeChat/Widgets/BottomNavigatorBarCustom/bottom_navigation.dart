4dd819b145bacd1974e53bce6ba99cb245a0c486
Quang <nghnquang2003@gmail.com>
2025-04-02 22:03:36 +0700

    chat feature

--- lib/View/HomeChat/Widgets/BottomNavigatorBarCustom/bottom_navigation.dart
+++ lib/View/HomeChat/Widgets/BottomNavigatorBarCustom/bottom_navigation.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
   final ValueChanged<int> onTap;
 
   const CustomBottomNavigationBar({
-    Key? key,
+    super.key,
     required this.currentIndex,
     required this.onTap,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +16,19 @@ class CustomBottomNavigationBar extends StatelessWidget {
       type: BottomNavigationBarType.fixed,
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.chat),
+          icon: Icon(Icons.chat_bubble_outline),
           label: 'Chat',
         ),
         BottomNavigationBarItem(
-          icon: Icon(FontAwesomeIcons.list),
-          label: 'Prompt',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(FontAwesomeIcons.robot),
+          icon: Icon(Icons.lightbulb_outline),
           label: 'Bot AI',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person),
+          icon: Icon(Icons.list),
+          label: 'Prompt',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
           label: 'Information',
         ),
       ],
