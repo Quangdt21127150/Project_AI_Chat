2d912a52df5633bc7eda8ba79406d7c655071b40
Quang <nghnquang2003@gmail.com>
2025-05-05 14:41:22 +0700

    Reduce file pubspec.yaml

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -19,6 +19,11 @@ class AuthService {
     try {
       final response = await dioAuth.post(
         '/auth/password/sign-up',
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        ),
         data: {
           'email': user.email,
           'password': user.password,
@@ -70,6 +75,11 @@ class AuthService {
     try {
       final response = await dioAuth.post(
         '/auth/password/sign-in',
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        ),
         data: {
           'email': email,
           'password': password,
@@ -166,12 +176,15 @@ class AuthService {
   Future<ApiResponse> logout() async {
     try {
       final prefs = await SharedPreferences.getInstance();
+      final accessToken = prefs.getString('accessToken');
       final refreshToken = prefs.getString('refreshToken');
 
       final response = await dioAuth.delete(
         '/auth/sessions/current',
         options: Options(
           headers: {
+            'Authorization': 'Bearer $accessToken',
+            'Content-Type': 'application/json',
             'X-Stack-Refresh-Token': refreshToken,
           },
         ),
